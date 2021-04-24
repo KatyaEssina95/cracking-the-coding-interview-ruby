8ba11afdefa4da6592682cd39189bb05aec8aebe
Katya Essina <katya@ophelos.com>
2021-04-24 10:50:23 +0100

    palindome permutation + one away

--- lib/arrays_and_strings.rb
+++ lib/arrays_and_strings.rb
@@ -1,3 +1,4 @@
+#1.1
 def string_has_unique_chars?(test_string)
   # use a hash to keep track of seen chars
   seen_chars = {}
@@ -30,6 +31,7 @@ def string_has_unique_chars_no_structures?(test_string)
   true
 end
 
+#1.2
 def is_permutation?(str_1, str_2)
   if str_1.length != str_2.length
     return false
@@ -38,7 +40,10 @@ def is_permutation?(str_1, str_2)
   str_1.chars.sort == str_2.chars.sort
 end
 
+#1.3
 def urlify(string_with_spaces)
+  # add enough spaces to an array at the end
+  # loop backwards and replace chars to not overwrite
   number_of_spaces = string_with_spaces.count ' '
   string_array = string_with_spaces.chars
   new_index = string_with_spaces.length - 1 + 2*number_of_spaces
@@ -56,4 +61,61 @@ def urlify(string_with_spaces)
     old_index -= 1
   end
   string_array.join
+end
+
+#1.4
+def palindrome_permutation?(test_string)
+  # hash
+  # all should be even
+  # there should be no more than 1 letter of odd freq
+  chars_hash = {}
+  odd_count = 0
+  test_string.downcase.gsub(/\s+/, "").each_char do |char|
+    if chars_hash.has_key? char
+      chars_hash[char] += 1
+    else
+      chars_hash[char] = 1
+    end
+
+    if chars_hash[char] % 2 == 0
+      odd_count -= 1
+    else
+      odd_count += 1
+    end
+  end
+  odd_count <= 1
+end
+
+#1.5
+def one_away?(string_1, string_2)
+  if (string_1.length - string_2.length).abs > 1
+    return false
+  end
+
+  if string_1.length >= string_2.length
+    long_string = string_1
+    short_string = string_2
+  else
+    long_string = string_2
+    short_string = string_1
+  end
+
+  difference = 0
+  long_pointer = 0
+  short_pointer = 0
+
+  while short_pointer < short_string.length do
+    if long_string[long_pointer] == short_string[short_pointer]
+      long_pointer += 1
+      short_pointer += 1
+    elsif long_string.length == short_string.length
+      difference += 1
+      long_pointer += 1
+      short_pointer += 1
+    else
+      difference += 1
+      long_pointer += 1
+    end
+  end
+  difference <= 1
 end
--- test/test_arrays_and_strings.rb
+++ test/test_arrays_and_strings.rb
@@ -31,4 +31,44 @@ class TestArraysAndStrings < Minitest::Test
     url = string_with_spaces.gsub! " ", "%20"
     assert_equal urlify(string_with_spaces), url
   end
+
+  def test_is_palindrome_permuation
+    palindrome_permuation = "taco cat"
+    assert palindrome_permutation? palindrome_permuation
+  end
+
+  def test_is_not_palindrome_permutation
+    not_palindrome_permutation = "another taco cat"
+    refute palindrome_permutation? not_palindrome_permutation
+  end
+
+  def test_one_away_remove
+    string_1 = "pale"
+    string_2 = "ple"
+    assert one_away? string_1, string_2
+  end
+  
+  def test_one_away_add
+    string_1 = "pales"
+    string_2 = "pale"
+    assert one_away? string_1, string_2
+  end
+  
+  def test_one_away_replace
+    string_1 = "pale"
+    string_2 = "bale"
+    assert one_away? string_1, string_2
+  end
+
+  def test_one_away_replace
+    string_1 = "pulp"
+    string_2 = "gulp"
+    assert one_away? string_1, string_2
+  end
+  
+  def test_more_than_one_away
+    string_1 = "pale"
+    string_2 = "bake"
+    refute one_away? string_1, string_2
+  end
 end
